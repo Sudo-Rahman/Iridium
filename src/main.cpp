ece94da9f0bf75126e239b65ef4768ecbd6a8c0b
Rahman Yilmaz <85690773+Sudo-Rahman@users.noreply.github.com>
2024-07-02 10:35:13 +0300

    crash when you specify bad rclone binary fix

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 			auto not_found = RcloneNotFoundWidget();
 			not_found.exec();
 			if (not not_found.rcloneInstalled())
-				exit(1);
+				exit(0);
 		}
 	});
 
